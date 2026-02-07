b864ff53d5257cb304ab31e28a38c739ecf477df
ohjh9701 <ohjh4875@gmail.com>
2026-02-07 22:15:54 +0900

    [생성] OhClinic 프로젝트 생성

---
+++ OhClinicProject/src/main/java/com/ohclinic/OhClinicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhClinicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhClinicProjectApplication.class, args);
+	}
+
+}
---
+++ OhClinicProject/src/test/java/com/ohclinic/OhClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
